62eb290554d7bd82656e57b6e1cc1605aa4f5d4d
js910924 <js910924@gmail.com>
2019-11-08 09:18:29 +0800

    [Jason] Problem 938 Solved

---
+++ 938_RangeSumOfBST/Jason/938.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+}
+
+func rangeSumBST(root *TreeNode, L int, R int, dic map[int]bool) int {
+	if (root.Left != nil && root.Right != nil) {
+        if (root.Val >= L && root.Val <= R) {
+            return rangeSumBST(root.Left, L, R) + rangeSumBST(root.Right, L, R) + root.Val
+        }
+        
+		return rangeSumBST(root.Left, L, R) + rangeSumBST(root.Right, L, R)
+	}
+    
+    if (root.Left != nil) {
+        if (root.Val >= L && root.Val <= R) {
+            return rangeSumBST(root.Left, L, R) + root.Val
+        }
+		return rangeSumBST(root.Left, L, R)
+	}
+
+	if (root.Right != nil) {
+        if (root.Val >= L && root.Val <= R) {
+            return rangeSumBST(root.Right, L, R) + root.Val
+        }
+		return rangeSumBST(root.Right, L, R)
+	}
+
+    if (root.Val >= L && root.Val <= R) {
+        return root.Val
+    }
+	
+    return 0
+}
+
+type TreeNode struce {
+	Val int
+	Left *TreeNode
+	Right *TreeNode
+}
---
+++ 938_RangeSumOfBST/Jason/README.md
@@ -0,0 +1,4 @@
+# 938. Range Sum of BST
+
+Runtime: 92 ms, faster than 93.43% of Go online submissions for Range Sum of BST. 
+Memory Usage: 8.1 MB, less than 87.50% of Go online submissions for Range Sum of BST.
