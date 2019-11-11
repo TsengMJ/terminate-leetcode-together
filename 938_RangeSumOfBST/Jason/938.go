ff174e6dde2ae7cc4802e08926d771fb9390faab
js910924 <js910924@gmail.com>
2019-11-11 10:52:24 +0800

    [Jason] Make Code Cleaner

--- 938_RangeSumOfBST/Jason/938.go
+++ 938_RangeSumOfBST/Jason/938.go
@@ -1,45 +1,61 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
 }
 
-func rangeSumBST(root *TreeNode, L int, R int, dic map[int]bool) int {
-    if (root.Left != nil && root.Right != nil) {
-        if (root.Val >= L && root.Val <= R) {
-            return rangeSumBST(root.Left, L, R) + rangeSumBST(root.Right, L, R) + root.Val
-        }
-        
-	return rangeSumBST(root.Left, L, R) + rangeSumBST(root.Right, L, R)
-    }
-    
-    if (root.Left != nil) {
-        if (root.Val >= L && root.Val <= R) {
-            return rangeSumBST(root.Left, L, R) + root.Val
-        }
-	return rangeSumBST(root.Left, L, R)
-    }
+func rangeSumBST(root *TreeNode, L int, R int) int {
+	// Version 2
+	if root == nil {
+		return 0
+	}
 
-    if (root.Right != nil) {
-        if (root.Val >= L && root.Val <= R) {
-            return rangeSumBST(root.Right, L, R) + root.Val
-        }
-	return rangeSumBST(root.Right, L, R)
-   }
+	if root.Val < L {
+		return rangeSumBST(root.Right, L, R)
+	}
 
-    if (root.Val >= L && root.Val <= R) {
-        return root.Val
-    }
-	
-    return 0
+	if root.Val > R {
+		return rangeSumBST(root.Left, L, R)
+	}
+
+	if root.Val >= L && root.Val <= R {
+		return rangeSumBST(root.Left, L, R) + rangeSumBST(root.Right, L, R) + root.Val
+	}
+
+	return 0
+
+	// Version 1
+	//     if (root.Left != nil && root.Right != nil) {
+	//         if (root.Val >= L && root.Val <= R) {
+	//             return rangeSumBST(root.Left, L, R) + rangeSumBST(root.Right, L, R) + root.Val
+	//         }
+
+	// 	return rangeSumBST(root.Left, L, R) + rangeSumBST(root.Right, L, R)
+	//     }
+
+	//     if (root.Left != nil) {
+	//         if (root.Val >= L && root.Val <= R) {
+	//             return rangeSumBST(root.Left, L, R) + root.Val
+	//         }
+	// 	return rangeSumBST(root.Left, L, R)
+	//     }
+
+	//     if (root.Right != nil) {
+	//         if (root.Val >= L && root.Val <= R) {
+	//             return rangeSumBST(root.Right, L, R) + root.Val
+	//         }
+	// 	return rangeSumBST(root.Right, L, R)
+	//    }
+
+	//     if (root.Val >= L && root.Val <= R) {
+	//         return root.Val
+	//     }
+
+	//     return 0
 }
 
-type TreeNode struce {
-	Val int
-	Left *TreeNode
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
 	Right *TreeNode
 }
--- 938_RangeSumOfBST/Jason/README.md
+++ 938_RangeSumOfBST/Jason/README.md
@@ -1,4 +1,9 @@
 # 938. Range Sum of BST
 
+## Version 1
 Runtime: 92 ms, faster than 93.43% of Go online submissions for Range Sum of BST. 
 Memory Usage: 8.1 MB, less than 87.50% of Go online submissions for Range Sum of BST.
+
+## Version 2
+Runtime: 84 ms, faster than 99.79% of Go online submissions for Range Sum of BST.
+Memory Usage: 8.3 MB, less than 37.50% of Go online submissions for Range Sum of BST.
