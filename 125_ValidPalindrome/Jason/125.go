ef4232e1975d34291c1e7aaf34f0d6cea2de38ff
js910924 <js910924@gmail.com>
2019-11-11 10:42:57 +0800

    [Jason] Make Code Faster

--- 125_ValidPalindrome/Jason/125.go
+++ 125_ValidPalindrome/Jason/125.go
@@ -11,26 +11,17 @@ func main() {
 }
 
 func IsPalindrome(s string) bool {
-	sample := make(map[int]bool)
-	for i := 48; i < 58; i++ {
-		sample[i] = true
-	}
-	for i := 65; i < 91; i++ {
-		sample[i] = true
-	}
-	
+	// Version 2
 	s = strings.ToUpper(s)
 	i := 0
 	j := len(s) - 1
 	for i < j {
-		number := int(s[i])
-		if _, ok := sample[number]; !ok {
+		if (s[i] < '0' || s[i] > '9') && (s[i] < 'A' || s[i] > 'Z') {
 			i++
 			continue
 		}
 
-		number = int(s[j])
-		if _, ok := sample[number]; !ok {
+		if (s[j] < '0' || s[j] > '9') && (s[j] < 'A' || s[j] > 'Z') {
 			j--
 			continue
 		}
@@ -44,4 +35,41 @@ func IsPalindrome(s string) bool {
 	}
 
 	return true
+
+	// Version 1
+	// sample := make(map[int]bool)
+
+	// for i := 48; i < 58; i++ {
+	// 	sample[i] = true
+	// }
+
+	// for i := 65; i < 91; i++ {
+	// 	sample[i] = true
+	// }
+
+	// s = strings.ToUpper(s)
+	// i := 0
+	// j := len(s) - 1
+	// for i < j {
+	// 	number := int(s[i])
+	// 	if _, ok := sample[number]; !ok {
+	// 		i++
+	//         continue
+	// 	}
+
+	// 	number = int(s[j])
+	// 	if _, ok := sample[number]; !ok {
+	// 		j--
+	//         continue
+	// 	}
+
+	// 	if s[i] != s[j] {
+	// 		return false
+	// 	}
+
+	// 	i++
+	// 	j--
+	// }
+
+	// return true
 }
--- 125_ValidPalindrome/Jason/README.md
+++ 125_ValidPalindrome/Jason/README.md
@@ -1,4 +1,9 @@
 # 125. Valid Palindrome
 
+## Version 1
 Runtime: 4 ms, faster than 70.55% of Go online submissions for Valid Palindrome.
 Memory Usage: 3.7 MB, less than 100.00% of Go online submissions for Valid Palindrome.
+
+## Version 2
+Runtime: 0 ms, faster than 100.00% of Go online submissions for Valid Palindrome.
+Memory Usage: 3 MB, less than 100.00% of Go online submissions for Valid Palindrome.
