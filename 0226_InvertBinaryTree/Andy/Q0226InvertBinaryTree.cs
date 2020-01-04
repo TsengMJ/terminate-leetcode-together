3e210e16aea2305c26b927a0bb3e3d0ff2dfdb3c
Andy Chou <dog30111@gmail.com>
2020-01-04 23:51:49 +0800

    [Andy] : 226

---
+++ 0226_InvertBinaryTree/Andy/Q0226InvertBinaryTree.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace LeetCode.Easy
+{
+    /*
+    Invert a binary tree.
+
+    Example:
+    Input:
+         4
+       /   \
+      2     7
+     / \   / \
+    1   3 6   9
+
+    Output:
+         4
+       /   \
+      7     2
+     / \   / \
+    9   6 3   1
+    */
+
+    public class Q0226InvertBinaryTree
+    {
+        public Node InvertTree(Node root)
+        {
+            if (root != null)
+            {
+                this.SwapNode(root);
+                this.InvertTree(root.Left);
+                this.InvertTree(root.Right);
+            }
+            return root;
+        }
+
+        private void SwapNode(Node node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException(nameof(node), "不可為null");
+            }
+
+            var temp = node.Left;
+            node.Left = node.Right;
+            node.Right = temp;
+        }
+    }
+}
