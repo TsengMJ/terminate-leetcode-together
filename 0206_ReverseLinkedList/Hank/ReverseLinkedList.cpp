878a347e8990db1c01bcdd2e7e981c537ca2e510
Hank <hank19960918@gmail.com>
2020-01-12 18:16:16 +0800

    ReverseLinkedList Finished

---
+++ 0206_ReverseLinkedList/Hank/ReverseLinkedList.cpp
@@ -0,0 +1,22 @@
+﻿/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+	ListNode* reverseList(ListNode* head) {
+		ListNode *pre = NULL, *curr = NULL;
+		while (head != NULL)
+		{
+			curr = head;
+			head = curr->next;
+			curr->next = pre;
+			pre = curr;
+		}
+		return pre;
+	}
+};
