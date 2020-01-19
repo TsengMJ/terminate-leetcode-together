3d80cd7ceffe87cec70af052df1b06fa8f3679ee
anniehung05911 <anniehung05911@gmail.com>
2020-01-19 16:47:16 +0800

    [Annie] complete problem 169 in c

--- 0169_MajorityElement/Annie/README.md
+++ 0169_MajorityElement/Annie/README.md
@@ -3,3 +3,12 @@
 ## Python Version
 Runtime: 156 ms, faster than 58.39% of Python online submissions for Majority Element.
 Memory Usage: 14.4 MB, less than 7.32% of Python online submissions for Majority Element.
+
+## C Version
+Solution 1 :
+Runtime: 16 ms, faster than 95.55% of C online submissions for Majority Element.
+Memory Usage: 8.9 MB, less than 100.00% of C online submissions for Majority Element.
+
+Solution 2 (clear):
+Runtime: 12 ms, faster than 99.60% of C online submissions for Majority Element.
+Memory Usage: 8.9 MB, less than 100.00% of C online submissions for Majority Element.
---
+++ 0169_MajorityElement/Annie/leetcode_0169_clear.c
@@ -0,0 +1,32 @@
+/*
+ * 169. Majority Element
+ *
+ * Given an array of size n, find the majority element. 
+ * The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+ *
+ * Input: [3,2,3]
+ * Output: 3
+ *
+ * Runtime: 12 ms, faster than 99.60% of C online submissions for Majority Element.
+ * Memory Usage: 8.9 MB, less than 100.00% of C online submissions for Majority Element.
+ */
+
+
+int majorityElement(int* nums, int numsSize){
+    int value = 0;
+    int count = 0;
+    for(int i=0; i<numsSize; i++){
+        if(count == 0){
+            value = nums[i];
+        }
+
+        if(value == nums[i]){
+            count ++;
+        }
+        else{
+            count --;
+        }
+    }
+    return value;
+}
+
