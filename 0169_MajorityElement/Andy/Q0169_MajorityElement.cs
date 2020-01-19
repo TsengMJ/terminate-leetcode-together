73537bf22086ff96d2d0109edb5a686a11578f1a
Andy Chou <dog30111@gmail.com>
2020-01-19 15:28:22 +0800

    [Andy] : 169

---
+++ 0169_MajorityElement/Andy/Q0169_MajorityElement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Easy
+{
+    public class Q0169_MajorityElement
+    {
+        public int MajorityElement(int[] nums)
+        {
+            if (nums == null)
+            {
+                throw new ArgumentNullException(nameof(nums), "不可為null");
+            }
+
+            if (nums.Length == 0)
+            {
+                throw new ArgumentException("不可為empty", nameof(nums));
+            }
+
+            var dictionary = new Dictionary<int, int>();
+            var targetCount = nums.Length / 2;
+
+            foreach (var num in nums)
+            {
+                if (dictionary.ContainsKey(num))
+                {
+                    dictionary[num] += 1;
+                }
+                else
+                {
+                    dictionary.Add(num, 1);
+                }
+
+                if (dictionary[num] > targetCount)
+                {
+                    return num;
+                }
+            }
+
+            throw new ArgumentException("沒有主要元素", nameof(nums));
+        }
+    }
+}
---
+++ 0169_MajorityElement/Andy/Q0169_MajorityElementTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using FluentAssertions;
+using LeetCode.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.EasyTests
+{
+    [TestClass()]
+    public class Q0169_MajorityElementTests
+    {
+        [TestMethod()]
+        public void MajorityElementTest_nums輸入323_應回傳3()
+        {
+            // arrange
+            var nums = new int[] { 3, 2, 3 };
+
+            var expect = 3;
+
+            var sut = new Q0169_MajorityElement();
+
+            // act
+            var actual = sut.MajorityElement(nums);
+
+            // assert
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod()]
+        public void MajorityElementTest_nums輸入2211122_應回傳2()
+        {
+            // arrange
+            var nums = new int[] { 2, 2, 1, 1, 1, 2, 2 };
+
+            var expect = 2;
+
+            var sut = new Q0169_MajorityElement();
+
+            // act
+            var actual = sut.MajorityElement(nums);
+
+            // assert
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod()]
+        public void MajorityElementTest_nums輸入1_應回傳1()
+        {
+            // arrange
+            var nums = new int[] { 1 };
+
+            var expect = 1;
+
+            var sut = new Q0169_MajorityElement();
+
+            // act
+            var actual = sut.MajorityElement(nums);
+
+            // assert
+            actual.Should().Be(expect);
+        }
+
+        [TestMethod()]
+        public void MajorityElementTest_nums為empty_應拋出ArgumentException()
+        {
+            // arrange
+            var nums = new int[] { };
+
+            var sut = new Q0169_MajorityElement();
+
+            // act
+            Action actual = () => sut.MajorityElement(nums);
+
+            // assert
+            actual.Should().Throw<ArgumentException>()
+                  .Which.Message.Should().Contain("不可為empty");
+        }
+
+        [TestMethod()]
+        public void MajorityElementTest_nums為null_應拋出ArgumentNullException()
+        {
+            // arrange
+            int[] nums = null;
+
+            var sut = new Q0169_MajorityElement();
+
+            // act
+            Action actual = () => sut.MajorityElement(nums);
+
+            // assert
+            actual.Should().Throw<ArgumentNullException>()
+                  .Which.Message.Should().Contain("不可為null");
+        }
+
+        [TestMethod()]
+        public void MajorityElementTest_nums為沒有主要元素_應拋出ArgumentException()
+        {
+            // arrange
+            var nums = new int[] { 1, 1, 1, 2, 2, 2 };
+
+            var sut = new Q0169_MajorityElement();
+
+            // act
+            Action actual = () => sut.MajorityElement(nums);
+
+            // assert
+            actual.Should().Throw<ArgumentException>()
+                  .Which.Message.Should().Contain("沒有主要元素");
+        }
+    }
+}
