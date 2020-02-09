9a15e379439846c98c640e25be6d12d8c865b340
Andy Chou <dog30111@gmail.com>
2020-02-09 16:55:15 +0800

    [Andy] : 21

---
+++ 0021_MergeTwoSortedLists/Andy/Q0021_MergeTwoSortedLists.cs
@@ -0,0 +1,39 @@
+﻿using LeetCode.Common;
+
+namespace LeetCode.Easy
+{
+    /*
+    Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+    Example:
+    Input: 1->2->4, 1->3->4
+    Output: 1->1->2->3->4->4
+    */
+
+    public class Q0021_MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+            {
+                return l2;
+            }
+
+            if (l2 == null)
+            {
+                return l1;
+            }
+
+            if (l1.Data < l2.Data)
+            {
+                l1.Next = MergeTwoLists(l1.Next, l2);
+            }
+            else
+            {
+                l2.Next = MergeTwoLists(l2.Next, l1);
+            }
+
+            return l1.Data < l2.Data ? l1 : l2;
+        }
+    }
+}
---
+++ 0021_MergeTwoSortedLists/Andy/Q0021_MergeTwoSortedListsTests.cs
@@ -0,0 +1,216 @@
+﻿using FluentAssertions;
+using LeetCode.Common;
+using LeetCode.Easy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.EasyTests
+{
+    [TestClass()]
+    public class Q0021_MergeTwoSortedListsTests
+    {
+        [TestMethod()]
+        public void MergeTwoLists_l1輸入124_l2輸入134_應回傳112344()
+        {
+            // arrange
+            var l1 = new ListNode
+            {
+                Data = 1,
+                Next = new ListNode
+                {
+                    Data = 2,
+                    Next = new ListNode
+                    {
+                        Data = 4
+                    }
+                }
+            };
+            var l2 = new ListNode
+            {
+                Data = 1,
+                Next = new ListNode
+                {
+                    Data = 3,
+                    Next = new ListNode
+                    {
+                        Data = 4
+                    }
+                }
+            };
+
+            var expect = new ListNode
+            {
+                Data = 1,
+                Next = new ListNode
+                {
+                    Data = 1,
+                    Next = new ListNode
+                    {
+                        Data = 2,
+                        Next = new ListNode
+                        {
+                            Data = 3,
+                            Next = new ListNode
+                            {
+                                Data = 4,
+                                Next = new ListNode
+                                {
+                                    Data = 4
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var sut = new Q0021_MergeTwoSortedLists();
+
+            // act
+            var actual = sut.MergeTwoLists(l1, l2);
+
+            // assert
+            actual.Should().BeEquivalentTo(expect);
+        }
+
+        [TestMethod()]
+        public void MergeTwoLists_l1輸入null_l2輸入null_應回傳null()
+        {
+            // arrange
+            ListNode l1 = null;
+            ListNode l2 = null;
+
+            var sut = new Q0021_MergeTwoSortedLists();
+
+            // act
+            var actual = sut.MergeTwoLists(l1, l2);
+
+            // assert
+            actual.Should().BeNull();
+        }
+
+        [TestMethod()]
+        public void MergeTwoLists_l1輸入0_l2輸入null_應回傳0()
+        {
+            // arrange
+            var l1 = new ListNode
+            {
+                Data = 0
+            };
+            ListNode l2 = null;
+
+            var expect = new ListNode
+            {
+                Data = 0
+            };
+
+            var sut = new Q0021_MergeTwoSortedLists();
+
+            // act
+            var actual = sut.MergeTwoLists(l1, l2);
+
+            // assert
+            actual.Should().BeEquivalentTo(expect);
+        }
+
+        [TestMethod()]
+        public void MergeTwoLists_l1輸入null_l2輸入0_應回傳0()
+        {
+            // arrange
+            ListNode l1 = null;
+            var l2 = new ListNode
+            {
+                Data = 0
+            };
+
+            var expect = new ListNode
+            {
+                Data = 0
+            };
+
+            var sut = new Q0021_MergeTwoSortedLists();
+
+            // act
+            var actual = sut.MergeTwoLists(l1, l2);
+
+            // assert
+            actual.Should().BeEquivalentTo(expect);
+        }
+
+        [TestMethod()]
+        public void MergeTwoLists_l1輸入2_l2輸入1_應回傳12()
+        {
+            // arrange
+            var l1 = new ListNode
+            {
+                Data = 2
+            };
+            var l2 = new ListNode
+            {
+                Data = 1
+            };
+
+            var expect = new ListNode
+            {
+                Data = 1,
+                Next = new ListNode
+                {
+                    Data = 2
+                }
+            };
+
+            var sut = new Q0021_MergeTwoSortedLists();
+
+            // act
+            var actual = sut.MergeTwoLists(l1, l2);
+
+            // assert
+            actual.Should().BeEquivalentTo(expect);
+        }
+
+        [TestMethod()]
+        public void MergeTwoLists_l1輸入5_l2輸入124_應回傳1245()
+        {
+            // arrange
+            var l1 = new ListNode
+            {
+                Data = 5
+            };
+            var l2 = new ListNode
+            {
+                Data = 1,
+                Next = new ListNode
+                {
+                    Data = 2,
+                    Next = new ListNode
+                    {
+                        Data = 4
+                    }
+                }
+            };
+
+            var expect = new ListNode
+            {
+                Data = 1,
+                Next = new ListNode
+                {
+                    Data = 2,
+                    Next = new ListNode
+                    {
+                        Data = 4,
+                        Next = new ListNode
+                        {
+                            Data = 5
+                        }
+                    }
+                }
+            };
+
+            var sut = new Q0021_MergeTwoSortedLists();
+
+            // act
+            var actual = sut.MergeTwoLists(l1, l2);
+
+            // assert
+            actual.Should().BeEquivalentTo(expect);
+        }
+    }
+}
